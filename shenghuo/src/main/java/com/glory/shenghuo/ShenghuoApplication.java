ea8997d268d18bba60ced3868c2942819244a70d
liuzhenrong <liu.zhenrong@jyall.com>
2017-10-11 20:52:03 +0800

    走起

---
+++ shenghuo/src/main/java/com/glory/shenghuo/ShenghuoApplication.java
@@ -0,0 +1,12 @@
+package com.glory.shenghuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShenghuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShenghuoApplication.class, args);
+	}
+}
---
+++ shenghuo/src/test/java/com/glory/shenghuo/ShenghuoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glory.shenghuo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShenghuoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
