8a9fa75e5ad1bcd3a9ef70850aec3a2e7513f3bf
liuzhenrong <liu.zhenrong@jyall.com>
2017-10-12 20:46:49 +0800

    uedit

---
+++ shenghuo/src/main/java/com/glory/shenghuo/api/user/pojo/UserPoJo.java
@@ -0,0 +1,14 @@
+package com.glory.shenghuo.api.user.pojo;
+
+import java.io.Serializable;
+
+public class UserPoJo implements Serializable{
+
+    private int id;
+    private String userId;
+    private String name;
+    private String pwd;
+    private String headImg;
+    private int userType;
+
+}
