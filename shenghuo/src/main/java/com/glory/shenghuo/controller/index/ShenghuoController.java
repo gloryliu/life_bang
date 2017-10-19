046bcd3d70018e4741a7fa515167fd6f30932eea
liuzhenrong <liu.zhenrong@jyall.com>
2017-10-19 13:04:37 +0800

    集成ueditor

--- shenghuo/src/main/java/com/glory/shenghuo/controller/index/ShenghuoController.java
+++ shenghuo/src/main/java/com/glory/shenghuo/controller/index/ShenghuoController.java
@@ -3,6 +3,7 @@ package com.glory.shenghuo.controller.index;
 import com.baidu.ueditor.ActionEnter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,22 +20,25 @@ public class ShenghuoController {
     }
 
     @RequestMapping("/ueditor/webPath")
-    public void webPath(String action,String noCache,HttpServletRequest request, HttpServletResponse response){
+    @ResponseBody
+    public String webPath(String action,String noCache,HttpServletRequest request, HttpServletResponse response){
         try {
 
-            request.setCharacterEncoding( "utf-8" );
-            response.setHeader("Content-Type" , "text/html");
-
-            String path = request.getContextPath();
-
-            PrintWriter out = response.getWriter();
-
-            out.write( new ActionEnter( request, path ).exec() );
+//            request.setCharacterEncoding( "utf-8" );
+//            response.setHeader("Content-Type" , "text/html");
+//
+//            String path = request.getContextPath();
+//
+//            PrintWriter out = response.getWriter();
+//
+//            out.write( new ActionEnter( request, path ).exec() );
 
         }catch (Exception e){
 
         }
 
+        String rootPath="static\\ueditor\\";
+        return new ActionEnter( request, rootPath ).exec();
 
     }
 
